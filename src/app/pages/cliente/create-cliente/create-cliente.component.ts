16d0c13c8b8509e45e70744b89fc0394583bf788
SebasGT08 <guaman.sebastian802@gmail.com>
2023-07-28 17:23:20 -0500

    Form Cliente

--- src/app/pages/cliente/create-cliente/create-cliente.component.ts
+++ src/app/pages/cliente/create-cliente/create-cliente.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Persona } from 'src/app/domain/persona.model';
 
 @Component({
   selector: 'app-create-cliente',
   templateUrl: './create-cliente.component.html',
   styleUrls: ['./create-cliente.component.scss']
 })
-export class CreateClienteComponent {
+export class CreateClienteComponent implements OnInit {
+  persona: Persona = {
+    cedula: '',
+    nombre: '',
+    telefono: '',
+    direccion: '',
+    correo: '',
+    tipo: 'C'
+  };
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(registerForm: { valid: any; }) {
+    if (!registerForm.valid) {
+      console.log('Formulario no válido');
+      return;
+    }
+
+    console.log(this.persona);
+  }
 }
