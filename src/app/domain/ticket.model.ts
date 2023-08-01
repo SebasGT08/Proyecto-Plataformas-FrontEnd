5b99c9c1a5d5c3411a412920271d5998b0cddcc7
SebasGT08 <guaman.sebastian802@gmail.com>
2023-08-01 15:47:49 -0500

    Guardar Ticket

--- src/app/domain/ticket.model.ts
+++ src/app/domain/ticket.model.ts
@@ -1,10 +1,15 @@
+import { Lugar } from "./lugar.model";
+import { Tarifa } from "./tarifa.model";
+import { Usuario } from "./usuario.model";
+import { Vehiculo } from "./vehiculo.model";
+
 export interface Ticket {
   ticketid?: number;
   fecha?: string;
   hora_entrada?: string;
   hora_salida?: string;
-  usuario?: string;
-  tarifa?: string;
-  lugar?: string;
-  vehiculo?: string;
+  usuario?: Usuario;
+  tarifa?: Tarifa;
+  lugar?: Lugar;
+  vehiculo?: Vehiculo;
 }
